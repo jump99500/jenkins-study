8b79ea694fa861495fcd349b51c96c85b59cc379
jump99500 <94940437+jump99500@users.noreply.github.com>
2022-09-01 09:53:13 +0900

    초기환경구성

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # jenkins-study
 젠킨스 공부를 위한 공간입니다.
 근데 너무 어려움..
+
+진행상황!!
+22/08/31
+Github Push -> Project-1/freestyle(webhook을 통해 배포서버에 git clone) -> Project-2/pipeline(Project-1이 실행되고 난 다음 실행되게끔 구성 - 실제배포) -> Slack과 연동
---
+++ Script/sh/jenkins-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo su
+sudo mkdir -p /home/ec2-user/docker
+suod cat >> /home/ec2-user/docker/jenkins.yaml <<-ENQ
+version: '3'
+
+services:
+    jenkins:
+        image: jenkins/jenkins:lts
+        container_name: jenkins_cicd
+        volumes:
+            - /var/run/docker.sock:/var/run/docker.sock
+            - /jenkins:/var/jenkins_home
+        ports:
+            - "8080:8080"
+        privileged: true
+        user: root
+ENQ
+
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo cd /home/ec2-user/docker/
+sudo docker-compose up -d
