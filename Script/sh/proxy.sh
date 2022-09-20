e7e8ceb8ad2d50b931d5916e303bf3dff4c69d57
jump99500 <94940437+jump99500@users.noreply.github.com>
2022-09-20 20:51:26 +0900

    09.20 11

--- Script/sh/proxy.sh
+++ Script/sh/proxy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-sudo yum install -y httpd
-
 sudo sh -c 'cat >> /etc/httpd/conf/httpd.conf << EOF
 <VirtualHost *:80>
 ProxyRequests Off
@@ -13,8 +11,8 @@ Order deny,allow
 Allow from all
 </proxy>
 
-ProxyPass / http://172.31.196.148:8080/
-ProxyPassReverse / http://172.31.196.148:8080/
+ProxyPass / http://172.31.196.142:8080/
+ProxyPassReverse / http://172.31.196.142:8080/
 </VirtualHost>
 EOF'
 
